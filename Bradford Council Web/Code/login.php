1cdc16c627e7ebd65c2718561655645c4e63afe2
bilalkh27 <158193524+bilalkh27@users.noreply.github.com>
2024-03-14 10:07:46 +0000

    Login system for user/admin
    Updated code for login system to allow access to admin panel. For Login.html file, password field is now masked.

--- Bradford Council Web/Code/login.php
+++ Bradford Council Web/Code/login.php
@@ -5,7 +5,7 @@ if ($connection === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-mysqli_select_db($connection, "bradford");
+mysqli_select_db($connection, "assets");
 
 // Define email and password variables
 $email = "";
@@ -19,15 +19,33 @@ if(isset($_POST['email']) && isset($_POST['password'])) {
 }
 
 // Query to check if email and password exist in the database
-$query = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+$query = "SELECT * FROM users WHERE email = '$email' AND password = '$password' AND status='Verified' ";
 $result = mysqli_query($connection, $query);
 
 if ($result) {
     // Check if any rows were returned
     if(mysqli_num_rows($result) > 0) {
+        $row=mysqli_fetch_assoc($result);
+        $type=$row['role'];
+        $_SESSION['login'] = true;
+       
+        $_SESSION['email'] = $row['email'];
+        $_SESSION['type'] = $row['role'];
+
+        switch ($type) {
+            case 'Admin':
+                 echo "<script>location.href='../Code/admin/index.php'</script>";
+                exit();
+                break;
+                case 'General User':
+                    echo "<script>location.href='home.html'</script>";
+                   exit();
+                   break;
+            // Add more cases if needed
+        }
         // Login successful, redirect to Home.html
-        header("Location: Home.html");
-        exit;
+        //header("Location: Home.html");
+        //exit;
     } else {
         // Login failed, redirect to Login.html
         header("Location: Login.html");
