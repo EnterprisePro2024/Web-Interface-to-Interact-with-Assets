5de3a0cb72365151db86a90659e955fb8bb7d545
MaariyahKn <Maariyah_khan1@outlook.com>
2024-04-02 16:39:02 +0100

    changes website initialisation page from home page to login.

--- Software/home.php
+++ Software/home.php
@@ -1,4 +1,9 @@
-<?php require_once("includes/setup.php"); ?>
+<?php 
+require_once("includes/setup.php");
+if (!isset($_SESSION['login']) || $_SESSION['login'] == false) {
+    header("Location: login.php");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
