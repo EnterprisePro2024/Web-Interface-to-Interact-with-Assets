3dfa9b34aaa94e7b71c214c2844e1c12ea078417
bilalkh27 <158193524+bilalkh27@users.noreply.github.com>
2024-04-29 11:07:15 +0100

    Hyperlinked FAQs text

--- Software/includes/footer.php
+++ Software/includes/footer.php
@@ -1,10 +1,11 @@
 
+
     <footer class="footer1">
         <p>Council Switchboard : 01274 432111</p>
         <p>Council Address : Britannia House, Hall Ings, Bradford BD1 1HX</p>
     </footer>
         
     <footer class="footer2">
-        <p>Accessibility Statement | FAQs</p>
-        <p class="p1">&copy; 2024 City Of Bradford Metropolitan District Council</p>
+    <p>Accessibility Statement | <a href="faqs.php"style="color: white;">FAQs</a></p>
+    <p class="p1">&copy; 2024 City Of Bradford Metropolitan District Council</p>
     </footer>
